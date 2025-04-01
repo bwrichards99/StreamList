aaa4c07055e437b70dcb11e0ac36ec4f98e1e0b0
bwrichards99 <brandon.richards3@student.uagc.edu>
2025-03-31 18:33:51 -0700

    Week 5

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,39 @@
+const CACHE_NAME = "streamlist-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/static/js/bundle.js",
+  "/icons/icon-192x192.png",
+  "/favicon.ico"
+];
+
+// Install event: Cache files
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("streamlist-cache").then((cache) => {
-      return cache.addAll(["/", "/index.html", "/manifest.json"]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache).catch(err => console.error("Cache add error:", err));
     })
   );
 });
 
+// Fetch event: Serve from cache or fetch new
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    fetch(event.request)
+      .then((response) => {
+        return response;
+      })
+      .catch(() => caches.match(event.request)) // Use cache if network fails
+  );
+});
+
+// Activate event: Cleanup old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
     })
   );
 });
